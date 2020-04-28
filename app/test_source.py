8f07881b01dec0daa3226c9fedc073bd774825f2
ChegeDaniella <chegedaniella@gmail>
2020-04-28 12:07:44 +0300

    adding the test files

---
+++ app/test_source.py
@@ -0,0 +1,25 @@
+import unittest
+from models import source
+
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    test class to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        run before every test
+        '''
+        self.new_source = Source(1234,"newyork")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))  
+
+    def test_init(self):
+        self.assertEqual(self.new_source.id,1234)  
+        self.assertEqual(self.new_source.name,"newyork") 
+
+if __name__ == '__main__':
+    unittest.main()            
